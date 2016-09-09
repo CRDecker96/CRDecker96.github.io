0af54d6a48f7a8856edc69a38ce09e8ef915b449
Cody R. Decker <lildecker6901@gmail.com>
2016-09-09 11:27:42 -0600

    Alien file

---
+++ sketch.js
@@ -0,0 +1,34 @@
+function setup() {
+  createCanvas(1000, 1000); 
+  background('#222222');
+  
+}
+
+function draw() {
+   
+  fill('rgba(0,255,0, 0.25)');
+ ellipse(450, 450, 600, 600);
+ line(400, 600, 500, 600);
+
+ fill('#222222')
+ ellipse(470, 450, 50, 30); 
+ ellipse(400, 450, 50, 30);
+ 
+fill('rgba(0,255,0, 0.25)');
+ellipse(470, 450, 30, 30); 
+ellipse(400, 450, 30, 30);
+ellipse(450, 500, 5, 10);
+ellipse(430, 500, 5, 10);
+
+fill(255, 204, 0);
+ ellipse(450, 130, 100, 100);
+ ellipse(450, 100, 100, 100);
+  ellipse(450, 90, 100, 100);
+   ellipse(450, 80, 100, 100);
+    ellipse(450, 70, 100, 100);
+     ellipse(450, 60, 100, 100);
+
+ 
+ 
+  
+}
