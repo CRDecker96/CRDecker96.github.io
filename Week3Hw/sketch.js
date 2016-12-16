70a624e516f779854b1cc2bdb93785c9b7b3387d
Cody Russell Decker <lildecker6901@gmail.com>
2016-12-16 12:22:29 -0700

    fixed stuff and links

--- Week3Hw/sketch.js
+++ Week3Hw/sketch.js
@@ -1,13 +1,13 @@
 var r;
 var g;
-var b; 
+var b;
 
 
-function set up () {
+function setup () {
   r = random (256);
   g = random (256);
-  b = random (256); 
+  b = random (256);
   createCanvas ( 1000, 1000);
   background (r,g,b);
-  
+
 }
